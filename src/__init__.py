a957df4f7b6e2c7782acf3fce7115ecdf18b1d11
MNetanel <20209534+MNetanel@users.noreply.github.com>
2024-02-11 01:07:09 +0200

    Start data preprocessing

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+
+RAW_DATA_DIR = Path('/media/netanel/Backup/Image Processing Course Project/data/raw')
+PREPROCESSED_DATA_DIR = Path('/media/netanel/Backup/Image Processing Course Project/data/preprocessed')
